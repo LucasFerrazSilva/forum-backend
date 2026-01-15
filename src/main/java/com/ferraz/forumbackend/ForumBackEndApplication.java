ba4d8aa475b4ca5ff999650f8dba1b2c9f244a12
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2026-01-15 17:55:10 -0300

    first commit

---
+++ src/main/java/com/ferraz/forumbackend/ForumBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ferraz.forumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumBackEndApplication.class, args);
+	}
+
+}
